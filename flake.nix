99f47f6b51d7e80ac6659624d4b7046e52bc1025
Anakin Childerhose <anakin.childerhose@savoirfairelinux.com>
2026-03-27 13:01:43 -0400

    add flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Anakin's C language server";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "anakins-c-ls";
+
+          packages = with pkgs; [
+            bats
+          ];
+        };
+      });
+}
