1136278065c0fbb2c879e359b4900dd46139b8bb
Anakin Childerhose <anakin.childerhose@savoirfairelinux.com>
2026-03-27 15:56:33 -0400

    Log LSP traffic to lsp.log in project directory

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@
 vim.opt.swapfile = false
 
 vim.lsp.set_log_level("trace")
+vim.lsp.log._set_filename(vim.fn.getcwd() .. "/lsp.log")
 
 vim.lsp.config("anakins-c-ls", {
     cmd = { "anakins-c-ls" },
